2d90eebe9329f0152723e7bbfee704fabd9c3ca6
Maxwell Ryan <weinbema@oregonstate.edu>
2017-08-28 15:22:05 -0600

    Add round overview information to bid entry view

---
+++ upDownRiver/bidHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  bidHeaderTableViewCell.swift
+//  upDownRiver
+//
+//  Created by bergerMacPro on 8/28/17.
+//  Copyright © 2017 bergerMacPro. All rights reserved.
+//
+
+import UIKit
+
+class bidHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
